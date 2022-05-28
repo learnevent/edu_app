ae662d47e238a4779d34bbbb4be70e713527986f
Hongru Ma <mahongru.dev@gmail.com>
2022-05-28 22:40:09 +0200

    Added progress indicator & disabled box detection

---
+++ lib/pages/homepage/format_label_list.dart
@@ -0,0 +1,24 @@
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+
+String formatLabelList(
+    // List<ImageLabel> labeledObjects, List<DetectedObject> detectedObjects) {
+    List<ImageLabel> labeledObjects) {
+  String textBody = '\n';
+
+  for (ImageLabel object in labeledObjects) {
+    var confidence = double.parse((object.confidence * 100).toStringAsFixed(1));
+    textBody = textBody + '[$confidence%] = ${object.label} \n';
+  }
+
+  // for (DetectedObject object in detectedObjects) {
+  //   var bottom = object.boundingBox.bottom;
+  //   var top = object.boundingBox.top;
+  //   var left = object.boundingBox.left;
+  //   var right = object.boundingBox.right;
+
+  //   textBody =
+  //       textBody + '\nBoundingBox \n  ($bottom, $top. $left, $right)  \n';
+  // }
+  return textBody;
+}
--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
-import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+// import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+import 'format_label_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +16,8 @@ class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
   XFile? imageFile;
+
+  bool checking = false;
 
   String scannedText = "";
 
@@ -29,7 +32,7 @@ class _HomePageState extends State<HomePage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              if (imageFile == null)
+              if (imageFile == null && checking == false)
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: const [
@@ -48,6 +51,7 @@ class _HomePageState extends State<HomePage> {
                   width: 600,
                   child: Image.file(File(imageFile!.path)),
                 ),
+              if (checking == true) const CircularProgressIndicator(),
               Text(
                 scannedText,
                 style: const TextStyle(fontSize: 25),
@@ -71,7 +75,6 @@ class _HomePageState extends State<HomePage> {
   void _changeIndex(int value) {
     _currentIndex = value;
     scannedText = "";
-    setState(() {});
 
     if (_currentIndex == 0) {
       processImage(ImageSource.gallery);
@@ -83,32 +86,38 @@ class _HomePageState extends State<HomePage> {
   }
 
   void processImage(ImageSource source) async {
-    final pickedImage = await ImagePicker().pickImage(source: source);
+    final imageLabeler =
+        ImageLabeler(options: ImageLabelerOptions(confidenceThreshold: 0.5));
+
+    // final objectDetector = ObjectDetector(
+    //     options: ObjectDetectorOptions(
+    //         mode: DetectionMode.singleImage,
+    //         classifyObjects: false,
+    //         multipleObjects: false));
+
     try {
+      imageFile = null;
+      checking = true;
+      setState(() {});
+
+      final pickedImage = await ImagePicker().pickImage(source: source);
+
       if (pickedImage != null) {
         imageFile = pickedImage;
-        setState(() {});
 
         final inputImage = InputImage.fromFilePath(pickedImage.path);
-
-        final imageLabeler = ImageLabeler(
-            options: ImageLabelerOptions(confidenceThreshold: 0.5));
 
         List<ImageLabel> labeledObjects =
             await imageLabeler.processImage(inputImage);
         await imageLabeler.close();
 
-        final objectDetector = ObjectDetector(
-            options: ObjectDetectorOptions(
-                mode: DetectionMode.singleImage,
-                classifyObjects: false,
-                multipleObjects: false));
+        // List<DetectedObject> detectedObjects =
+        //     await objectDetector.processImage(inputImage);
+        // await objectDetector.close();
 
-        List<DetectedObject> detectedObjects =
-            await objectDetector.processImage(inputImage);
-        await objectDetector.close();
-
-        scannedText = formatLabelsList(labeledObjects, detectedObjects);
+        // scannedText = formatLabelList(labeledObjects, detectedObjects);
+        scannedText = formatLabelList(labeledObjects);
+        checking = false;
         setState(() {});
       }
     } catch (e) {
@@ -116,27 +125,6 @@ class _HomePageState extends State<HomePage> {
       scannedText = "Error occured while scanning";
       setState(() {});
     }
-  }
-
-  String formatLabelsList(labeledObjects, detectedObjects) {
-    String textBody = '\n';
-
-    for (ImageLabel object in labeledObjects) {
-      var confidence =
-          double.parse((object.confidence * 100).toStringAsFixed(1));
-      textBody = textBody + '[$confidence%] = ${object.label} \n';
-    }
-
-    for (DetectedObject object in detectedObjects) {
-      var bottom = object.boundingBox.bottom;
-      var top = object.boundingBox.top;
-      var left = object.boundingBox.left;
-      var right = object.boundingBox.right;
-
-      textBody =
-          textBody + '\nBoundingBox \n  ($bottom, $top. $left, $right)  \n';
-    }
-    return textBody;
   }
 
   @override
