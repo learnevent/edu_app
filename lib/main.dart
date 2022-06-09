941392cd63d1f8c1fd45ceac053620c737ac8b18
Hongru Ma <mahongru.dev@gmail.com>
2022-06-09 09:47:48 +0200

    Added provider wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:edu_app/utils/colors.dart';
 import 'package:edu_app/route_generator.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// We create a "provider", which will store a value (here "Hello world").
+// By using a provider, this allows us to mock/override the value exposed.
+final exampelProvider = Provider((_) => 'Hello riverpod');
 
 void main() {
   runApp(
-    const MyApp(),
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    const ProviderScope(child: MyApp()),
   );
 }
 
-class MyApp extends StatelessWidget {
+// Note: MyApp is a HookConsumerWidget, from hooks_riverpod.
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Learn Event',
       theme:
           ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
       onGenerateRoute: (RouteSettings settings) =>
